000cffdb32cb64deec68a8d9f0221be57d655ca6
NazarovNikita1 <145589126+NazarovNikita1@users.noreply.github.com>
2023-12-20 01:39:23 +1000

    Add files via upload

---
+++ 1dz19dec.py
@@ -0,0 +1,38 @@
+from collections import Counter
+
+class NERStats:
+    def __init__(self, document, ner_extractor):
+        self.document = document
+        self.ner_extractor = ner_extractor
+        self.ner_entities = []
+
+    def analyze_entities(self):
+        self.ner_entities = self.ner_extractor.extract_entities(self.document)
+
+    def display_most_common_entities(self, n):
+        if not self.ner_entities:
+            print("Run analyze_entities first.")
+            return
+
+        #словарь
+        entity_counter = Counter(self.ner_entities)
+
+        most_common_entities = entity_counter.most_common(n)
+        print(f"Top {n} Most Common Named Entities:")
+        for entity, count in most_common_entities:
+            print(f"{entity}: {count}")
+
+# Пример использования:
+class NERExtractor:
+    def extract_entities(self, document):
+        pass
+
+# Создаем экземпляр NERStats
+document = "This one will be a document with named entities like IBM, Microsoft, and Facebook."
+ner_extractor = NERExtractor()
+ner_stats = NERStats(document, ner_extractor)
+
+ner_stats.analyze_entities()
+
+#отображение 3 самых частотных именованных сущностей
+ner_stats.display_most_common_entities(3)
